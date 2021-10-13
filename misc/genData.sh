c266ee992881eb7221a155161a0cbc77dfb3c72f
Zoom <zoom@zoom.com>
2021-10-13 17:55:54 +0200

    minor fix, remove useless flag from grep

--- misc/genData.sh
+++ misc/genData.sh
@@ -9,7 +9,7 @@ printf '#include <stdio.h>
 #include "hashmap.h"
 int main(void) { map_t *hashtable = createMap(10);\n'
 
-grep -m "$MAXLINES" -Ezaoi '[a-z]' /dev/urandom | tr -d '\0' |
+grep -zaoim "$MAXLINES" '[a-z]' /dev/urandom | tr -d '\0' |
     fold -w "$numChars" | nl -w 1 -s ' ' |
     sed -E 's/([[:digit:]]*)\s(.*)/insert\(\&hashtable, "KEY\1", "\2"\);/g' |
     head -n "$numLines"
